7e21f3011a53bd7782d7a56d6936e0b30262fd00
Yue Zhang <zhangyue.dev@outlook.com>
2021-07-23 10:16:26 +0800

    Global serilog

--- EPlusActivities.API/Program.cs
+++ EPlusActivities.API/Program.cs
@@ -1,29 +1,57 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using EPlusActivities.API.Entities;
-using EPlusActivities.API.Infrastructure;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace EPlusActivities.API
 {
     public class Program
     {
+        public static IConfiguration Configuration
+        {
+            get;
+        } =
+            new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json",
+                             optional: false,
+                             reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                             optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
         public static void Main(string[] args)
         {
+            Log.Logger =
+                new LoggerConfiguration()
+                    .ReadFrom.Configuration(Configuration)
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
+
+            try
+            {
+                Log.Information("启动主机");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "主机意外终止");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host
                 .CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
