6791c36f9a7f6ec8ad7458e8c7a20c6007a71f1e
Kamasylvia <contact@kamasylvia.com>
2021-10-08 16:28:11 +0800

    Update Lottery.DateTime

--- EPlusActivities.API/Migrations/ApplicationDb/20211008082634_ApplicationDbMigration2.Designer.cs
+++ EPlusActivities.API/Migrations/ApplicationDb/20211008082634_ApplicationDbMigration2.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using EPlusActivities.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EPlusActivities.API.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211008082634_ApplicationDbMigration2")]
+    partial class ApplicationDbMigration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
