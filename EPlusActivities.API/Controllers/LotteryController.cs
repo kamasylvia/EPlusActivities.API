89f5483c5779d5cbcb1865753ed2962532a1736d
Kamasylvia <contact@kamasylvia.com>
2021-10-12 16:24:21 +0800

    Format source with CSharpier

--- EPlusActivities.API/Controllers/LotteryController.cs
+++ EPlusActivities.API/Controllers/LotteryController.cs
@@ -237,7 +237,7 @@ namespace EPlusActivities.API.Controllers
                 generalLotteryRecords,
                 lotteries
             );
-            
+
             return File(memoryString, contentType);
         }
 
