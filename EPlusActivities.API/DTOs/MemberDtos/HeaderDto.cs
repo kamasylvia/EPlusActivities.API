04d62c370efc082038e5474bc0ad70c7eb5db4f8
Yue Zhang <zhangyue.dev@outlook.com>
2021-10-12 16:41:13 +0800

    git config core.ignorecase false

--- EPlusActivities.API/DTOs/MemberDtos/HeaderDto.cs
+++
@@ -1,9 +0,0 @@
-namespace EPlusActivities.API.Dtos.MemberDtos
-{
-    public class HeaderDto
-    {
-        public string Code { get; set; }
-
-        public string Message { get; set; }
-    }
-}
