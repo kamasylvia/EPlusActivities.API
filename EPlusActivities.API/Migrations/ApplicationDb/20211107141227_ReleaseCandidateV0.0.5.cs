c67bcb2771d9d0c9b1f994a2355dbdc52acb55ba
Yue Zhang <zhangyue.dev@outlook.com>
2021-11-07 22:13:01 +0800

    Update migration

---
+++ EPlusActivities.API/Migrations/ApplicationDb/20211107141227_ReleaseCandidateV0.0.5.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlusActivities.API.Migrations.ApplicationDb
+{
+    public partial class ReleaseCandidateV005 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GeneralLotteryRecords_DateTime",
+                table: "GeneralLotteryRecords");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_GeneralLotteryRecords_Channel_DateTime",
+                table: "GeneralLotteryRecords",
+                columns: new[] { "Channel", "DateTime" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_GeneralLotteryRecords_Channel_DateTime",
+                table: "GeneralLotteryRecords");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeneralLotteryRecords_DateTime",
+                table: "GeneralLotteryRecords",
+                column: "DateTime",
+                unique: true);
+        }
+    }
+}
