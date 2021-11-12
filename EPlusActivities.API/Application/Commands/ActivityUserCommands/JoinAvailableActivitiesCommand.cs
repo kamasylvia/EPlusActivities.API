050e5415dd9cc88a71b96e42687e96bd371ec2bd
Yue Zhang <zhangyue.dev@outlook.com>
2021-11-12 14:46:14 +0800

    Release candidate v20211112

--- EPlusActivities.API/Application/Commands/ActivityUserCommands/JoinAvailableActivitiesCommand.cs
+++ EPlusActivities.API/Application/Commands/ActivityUserCommands/JoinAvailableActivitiesCommand.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 using EPlusActivities.API.Dtos.ActivityUserDtos;
 using EPlusActivities.API.Infrastructure.Enums;
 using MediatR;
