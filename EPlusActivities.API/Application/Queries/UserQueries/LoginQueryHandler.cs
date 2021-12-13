51cab235e6f7f27fbd7f67ef470a6ff9314d7bba
Yue Zhang <zhangyue.dev@outlook.com>
2021-12-13 15:21:58 +0800

    Remove login actor method

--- EPlusActivities.API/Application/Queries/UserQueries/LoginQueryHandler.cs
+++ EPlusActivities.API/Application/Queries/UserQueries/LoginQueryHandler.cs
@@ -2,9 +2,6 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
-using Dapr.Actors;
-using Dapr.Actors.Client;
-using EPlusActivities.API.Application.Actors.UserActors;
 using EPlusActivities.API.Dtos.UserDtos;
 using EPlusActivities.API.Entities;
 using EPlusActivities.API.Infrastructure.Exceptions;
@@ -14,25 +11,55 @@ using Microsoft.AspNetCore.Identity;
 
 namespace EPlusActivities.API.Application.Queries.UserQueries
 {
-    public class LoginQueryHandler : IRequestHandler<LoginQuery, UserDto>
+    public class LoginQueryHandler : UserRequestHandlerBase, IRequestHandler<LoginQuery, UserDto>
     {
-        private readonly IActorProxyFactory _actorProxyFactory;
+        private readonly IMemberService _memberService;
+        private readonly IMapper _mapper;
 
-        public LoginQueryHandler(IActorProxyFactory actorProxyFactory)
+        public LoginQueryHandler(
+            UserManager<ApplicationUser> userManager,
+            IMapper mapper,
+            IMemberService memberService
+        ) : base(userManager)
         {
-            _actorProxyFactory =
-                actorProxyFactory ?? throw new ArgumentNullException(nameof(actorProxyFactory));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _memberService =
+                memberService ?? throw new ArgumentNullException(nameof(memberService));
         }
 
-        public async Task<UserDto> Handle(
-            LoginQuery request,
-            CancellationToken cancellationToken
-        ) =>
-            await _actorProxyFactory
-                .CreateActorProxy<IUserActor>(
-                    new ActorId(request.UserId.ToString() + request.ChannelCode.ToString()),
-                    nameof(UserActor)
-                )
-                .Login(request);
+        public async Task<UserDto> Handle(LoginQuery request, CancellationToken cancellationToken)
+        {
+            #region Parameter validation
+            var user = await _userManager.FindByIdAsync(request.UserId.ToString());
+            if (user is null)
+            {
+                throw new NotFoundException("Could not find the user.");
+            }
+            #endregion
+
+            #region Get member info
+            if ((await _userManager.GetRolesAsync(user)).Contains("customer"))
+            {
+                var memberDto = await _memberService.GetMemberAsync(
+                    user.PhoneNumber,
+                    request.ChannelCode
+                // Enum.Parse<ChannelCode>(request.ChannelCode, true)
+                );
+                user.IsMember = true;
+                user.MemberId = memberDto.Body.Content.MemberId;
+                user.Credit = memberDto.Body.Content.Points;
+            }
+            #endregion
+
+            #region Update the user
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                throw new DatabaseUpdateException();
+            }
+            #endregion
+
+            return _mapper.Map<UserDto>(user);
+        }
     }
 }
